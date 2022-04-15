41394dea689f9664eb84ed163ba79dee58564188
Chan531 <chan657312@gmail.com>
2022-04-15 10:57:02 +0900

    언타이틀 알고리즘 스터디 3주차 Level 1 설탕 배달

---
+++ 2839.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int sugar(int n)
+{
+	int i = n / 5;
+
+	while (i >= 0)
+	{
+		if (((n - (i * 5)) % 3) == 0)
+			return i + ((n - (i * 5)) / 3);
+		i--;
+	}
+
+	return -1;
+}
+
+int main(void)
+{
+	int n;
+	cin >> n;
+	cout << sugar(n);
+	return 0;
+}
